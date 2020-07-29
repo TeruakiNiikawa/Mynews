c6f09944a1ed68628e47b97cbdeed4dfbae10265
teruaki niikawa <n.teruaki.2301@icloud.com>
2020-07-29 13:39:04 +0000

    課題提出

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,9 @@ Route::group(['prefix' => 'admin'], function(){
 'Admin\NewsController@add');
 });
 
-Route::group(['prefix' => 'XXX'], function(){
-    Route::get('news/create',
-'AAA\BBB@add');
-});
+Route::get('/XXX','AAAController@bbb');
 
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('profile/create',
-'Admin\ProfileController@add');
-});
-
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('profile/edit',
-'Admin\ProfileController@edit');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
 });
