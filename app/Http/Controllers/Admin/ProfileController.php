3d964fca78ecd9bcb0f1032a6ba9c73c083abdd3
teruaki niikawa <n.teruaki.2301@icloud.com>
2020-08-04 01:34:20 +0000

    課題提出

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -10,6 +10,12 @@ class ProfileController extends Controller
     //
     public function add()
     {
+        $this->validate($request, Profile::$rules);
+​       $profile = new Profile;
+        $form = $request->all();
+        unset($form['_token']);
+        $profile->fill($form);
+        $profile->save();
         return view('admin.profile.create');
     }
     
--- app/Profile.php
+++ app/Profile.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Profile extends Model
+class profile extends Model
 {
     protected $guarded = array('id');
 
