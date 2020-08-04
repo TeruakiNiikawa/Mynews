916d45fa2cb940a48af40e851a297cbbeb4b6a49
teruaki niikawa <n.teruaki.2301@icloud.com>
2020-08-04 01:40:03 +0000

    課題提出

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -10,17 +10,17 @@ class ProfileController extends Controller
     //
     public function add()
     {
-        $this->validate($request, Profile::$rules);
-​       $profile = new Profile;
-        $form = $request->all();
-        unset($form['_token']);
-        $profile->fill($form);
-        $profile->save();
         return view('admin.profile.create');
     }
     
     public function create()
     {
+        $this->validate($request, profile::$rules);
+​       $profile = new profile;
+        $form = $request->all();
+        unset($form['_token']);
+        $profile->fill($form);
+        $profile->save();
         return redirect('admin/profile/create');
     }
     
